81acbe698882da6a033fc57384cf0e3da0632e5c
ClichyMercury <gaelelscientifico@outlook.com>
2022-05-07 09:05:52 +0000

    ajout projet flutter immobilier

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:projone/conponents/inscriptionACHETEUR.dart';
+import 'package:projone/conponents/inscriptionVENDEUR.dart';
+import 'package:projone/pages/page1.dart';
+import 'package:projone/pages/page2.dart';
+import 'package:projone/pages/profilepage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'domArt realEstate',
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+      ),
+      home: Page2(),
+    );
+  }
+}
